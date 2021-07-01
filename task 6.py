b97d5919b67c07ab2d7585bd533223a4ce73bc8f
Damotsov <damotsoff@gmail.com>
2021-07-01 09:57:48 +0300

    task6

---
+++ task 6.py
@@ -0,0 +1,15 @@
+from itertools import count
+from itertools import cycle
+for i in count(1):
+    if i == 50:
+        break
+    else:
+        print(i)
+##########################################################
+c=0
+for i in cycle('rep'):
+    if c>10:
+        break
+    print(i)
+    c += 1
+
